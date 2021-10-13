18370cb10cf708f446c8838b1f78ad22da233702
Gabriel Gilling <45206492+gabgilling@users.noreply.github.com>
2021-10-13 14:01:00 -0400

    Update and rename create_demographic_df.py to create_base_df.py

--- create_base_df.py
+++ create_base_df.py
@@ -30,19 +30,22 @@ demographic_cols = [
 # create subsetted df
 df_demographics = df[demographic_cols].copy()
 
+# impute numerical missing data
 df_demographics['Age_at_V1'] = df_demographics['Age_at_V1'].fillna(df_demographics['Age_at_V1'].mean())
 df_demographics['BMI'] = df_demographics['BMI'].fillna(df_demographics['BMI'].mean())
 
+# impute categorical missing data
 for cat_col in ['Education', 'GravCat', 'SmokeCat1', 'SmokeCat2', 'Ins_Govt', 'Ins_Mil', 
                 'Ins_Comm', 'Ins_Pers', 'Ins_Othr', 'V1AG11', 'V1AG01', 'V1AF14']:
     mode = df_demographics[cat_col].mode().values[0]
     df_demographics[cat_col] = df_demographics[cat_col].fillna(mode)
 
-# impute and standardize numeric features
+# standardize numeric features
 df_demographics[['GAwks_screen','Age_at_V1', 'BMI']] = df_demographics[['GAwks_screen','Age_at_V1', 'BMI']].apply(lambda x: (x - np.mean(x)) / np.std(x))
 
 df_demographics[[code for code in df_demographics.columns if code not in ['GAwks_screen','Age_at_V1', 'BMI']]] = df_demographics[[code for code in df_demographics.columns if code not in ['GAwks_screen','Age_at_V1', 'BMI']]].astype(str)
 
 df_demographics = pd.concat([df_demographics, df.PublicID], axis = 1)
 
+# save file
 df_demographics.to_pickle('df_demographics.pkl')
